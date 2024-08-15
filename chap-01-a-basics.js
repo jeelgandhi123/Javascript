c7a3bde5ae2a196c34d8c9192ca8cfc125308486
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-08-15 21:15:57 +0530

    Update chap-01-a-basics.js

--- chap-01-a-basics.js
+++ chap-01-a-basics.js
@@ -13,6 +13,22 @@ console.log(typeof a)
 console.log(typeof x)
 console.log(typeof fullname)
 
+
+// let var and const in js 
+var example1=23;
+let example2=34;
+{
+    var example1=24;
+    let example2=42;
+    console.log(example1,example2);//it will give 24 and 42
+    // let is block constrain
+}
+console.log(example1,example2);//it will give 24 and 34
+
+const example4="string"
+// const example4=12; this will be error............
+
+
 // object in js 
 const class_1={
     name:"jeel",
@@ -23,4 +39,7 @@ console.log(class_1);
 console.log(class_1.name);
 class_1.idnumber=121;
 console.log(class_1.idnumber);
+
+
+
 
