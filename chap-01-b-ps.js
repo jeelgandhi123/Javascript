276162ee316b0c9a382294e474af1f463c424b43
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-08-15 21:18:12 +0530

    Create chap-01-b-ps.js

---
+++ chap-01-b-ps.js
@@ -0,0 +1,18 @@
+// Create a const object called “product” to store information
+const product={
+    information_about_product:"black color pen",
+    star:4.0,
+    deal_of_the_day:true,
+    MRP:230
+}
+console.log(product)
+
+// Create a const object called “profile” to store information
+const profile={
+    user:"jeel gandhi",
+    isfollow:true,
+    post:197,
+    followers:54600,
+    following:4
+}
+console.log(profile)
