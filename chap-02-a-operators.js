3a89a5931ec12e33bedbc7ef1056a50226fa6c9f
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-08-15 21:27:16 +0530

    Create chap-02-a-operators.js

---
+++ chap-02-a-operators.js
@@ -0,0 +1,30 @@
+// operators
+let a=12,b=23;
+// arithmatic operator
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);//a^b
+console.log(a++);//print 12
+console.log(a)//print 13
+console.log(a--);//print 13
+console.log(a);//print 12
+
+// assignment operator
+let x=23;
+console.log(x+=1);//x=x+1
+
+// comparison operator
+if(a==12){
+    console.log("true")
+}
+if(b!=12){
+    console.log("flase")
+}
+
+// logical operator 
+// && ||
+console.log("a==12 && b==23 :",a==12 && b==23);
+console.log("a==12 || b==23 :",a==12 || b==23);
