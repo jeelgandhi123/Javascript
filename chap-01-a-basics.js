82a84de0e0b9d3efd26b485ecdc5f651cf4b3e5e
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-08-15 21:10:46 +0530

    Create chap-01-a-basics.js

---
+++ chap-01-a-basics.js
@@ -0,0 +1,26 @@
+// variables in js 
+age=23;
+radius=3.14;
+isfollow=true;
+a=null;
+x=undefined;
+fullname="jeel";
+
+console.log(typeof age)
+console.log(typeof radius)
+console.log(typeof isfollow)
+console.log(typeof a)
+console.log(typeof x)
+console.log(typeof fullname)
+
+// object in js 
+const class_1={
+    name:"jeel",
+    idnumber:101,
+    marks:199
+}
+console.log(class_1);
+console.log(class_1.name);
+class_1.idnumber=121;
+console.log(class_1.idnumber);
+
