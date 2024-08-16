baffade07078f89ffe380d86fde7ac9a95927ef2
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-08-16 21:28:53 +0530

    Create chap-06-a-function.js

---
+++ chap-06-a-function.js
@@ -0,0 +1,19 @@
+// function in js 
+// without parameter 
+function printhello(){
+    console.log("hello");
+}
+printhello();
+
+// with parameter 
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(23,32));
+
+// another way 
+const printname=()=>{
+    let fullname=prompt("enter your name");
+    console.log(fullname);
+}
+printname();
