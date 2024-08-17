48776d3766022d7d3b4fa4c64d76cd9e1c6f804f
Jeel Gandhi <152645970+jeelgandhi123@users.noreply.github.com>
2024-08-17 15:37:52 +0530

    Update chap-06-c-forEach.js

--- chap-06-c-forEach.js
+++ chap-06-c-forEach.js
@@ -1,20 +1,23 @@
-// foreach loop in array 
 let arr=[23,11,231,321,34];
-arr.forEach((value) => {
+const printvalue=(value)=>{
     console.log(value);
-});
+}
+arr.forEach(printvalue);
+
 
 let cricketer=["virat","rohit","kane"];
-
 cricketer.forEach((val,index,cricketer)=>{
     console.log(index,val.toUpperCase(),cricketer);
 });
+
 
 // practise set 
 // For a given array of numbers, print the square of each value using the forEach loop.
 
 let number=[22,34,100,43,66,78,43,21,40];
 
-number.forEach((value)=>{
-    console.log(value*value);
-});
+const countsquareroot=(value)=>{
+    console.log(value*value)
+}
+
+number.forEach(countsquareroot);
